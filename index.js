24462673e2d688d05ace8cdf15fbf6c82e2420ba
Emine Shaban <emineshaban@icloud.com>
2022-09-26 16:20:03 +0300

    Add routes

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 let express = require('express')
+let hbs = require('express-handlebars')
+
 let { PORT } = require('./config/env')
+const routes = require('./routes')
 const app = express()
+
+app.engine('hbs', hbs.engine({
+    extname: 'hbs'
+}))
+
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({extended : false}))
+app.use(express.static('public'))
+app.use(routes)
+
+
+
+
+
+
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
