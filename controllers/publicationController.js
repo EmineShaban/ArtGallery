aff94e08e1ee5d9848e5715f7b91f13fdcbd12f5
Emine Shaban <emineshaban@icloud.com>
2022-09-29 17:40:44 +0300

    details page

--- controllers/publicationController.js
+++ controllers/publicationController.js
@@ -3,12 +3,19 @@ const router = require('express').Router()
 const { isAuth } = require('../middlewares/authMiddleware')
 const publicationServices = require('../services/publicationServices')
 
-router.use(isAuth)
+router.get('/', async (req, res) => {
+    const publication = await publicationServices.getAll().lean()
+    res.render('publication', {publication})
+})
+router.get('/:publicationID/details', async (req, res) => {
+    const publication = await publicationServices.getOneDetailed(req.params.publicationID).lean()
+    res.render('publication/details', {...publication})
+})
 
-router.get('/create', (req, res) => {
+router.get('/create',isAuth, (req, res) => {
     res.render('publication/create')
 })
-router.post('/create', async (req, res) => {
+router.post('/create', isAuth, async (req, res) => {
     const createPublication = await publicationServices.create({...req.body, author: req.user._id})
 
     res.redirect('/publication')
--- services/publicationServices.js
+++ services/publicationServices.js
@@ -1,3 +1,6 @@
 const Publication = require('../models/Publication')
 
 exports.create = (publicationData) => Publication.create(publicationData)
+exports.getAll = () => Publication.find()
+exports.getOne = (publicationID) => Publication.findById(publicationID)
+exports.getOneDetailed = (publicationID) => Publication.findById(publicationID).populate('author')
