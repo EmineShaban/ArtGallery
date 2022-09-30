70d156a61bafe6159835c42b8dbc5701d31db561
Emine Shaban <emineshaban@icloud.com>
2022-09-30 22:12:11 +0300

    Profile page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,14 +1,21 @@
 // const { isAuth } = require('../middlewares/authMiddleware')
 
 const router = require('express').Router()
+const publicationServices = require('../services/publicationServices')
+const userService = require('../services/userService')
 
-router.get('/', (req, res) => {
-    res.render('home')
+router.get('/', async(req, res) => {
+const public = await publicationServices.getAll().lean()
+const publication = public.map(x => ({...x, shareCount: x.usersShared.length}))
+    res.render('home', {publication})
 })
 
-// router.get('/secret', isAuth, (req, res) => {
-//     res.send('The chamber of srcrets')
-// })
+router.get('/profile',async (req, res) => {
+   const user = await userService.getOne(req.user._id).populate('publication').populate('shares').lean()
+   const publicationTitles = user.publication.map(x => x.title).join(', ')
+   const sharedTitles = user.shares.map(x => x.title).join(', ')
+    res.render('home/profile', {...user, publicationTitles, sharedTitles})
+})
 
 
 module.exports = router
